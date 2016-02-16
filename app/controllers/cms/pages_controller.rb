60839142d26df9086c4cf5f110ebe07ae73433d1
Sergey Novikov <novikov359@gmail.com>
2016-02-16 23:45:39 +0300

    Add filter for columns for pages controller

--- app/controllers/cms/pages_controller.rb
+++ app/controllers/cms/pages_controller.rb
@@ -1,4 +1,11 @@
 module Cms
   class PagesController < ResourcesController
+    private
+
+      def columns_for_list
+        @columns_for_list = @resource.columns.select do |column|
+          %w(id title slug body created_at updated_at).include?(column.name)
+        end
+      end
   end
 end
