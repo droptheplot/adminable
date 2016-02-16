652e94261ead645c695bc8230ef116be9ad30bff
Sergey Novikov <novikov359@gmail.com>
2016-02-16 18:29:47 +0300

    Add index action for resources

--- app/controllers/cms/resources_controller.rb
+++ app/controllers/cms/resources_controller.rb
@@ -1,6 +1,15 @@
 module Cms
   class ResourcesController < ApplicationController
+    before_filter :set_resource
+
     def index
+      @entries = @resource.all
     end
+
+    private
+
+      def set_resource
+        @resource = params[:controller].classify.safe_constantize
+      end
   end
 end
