6eaf05c75a02e1cc6d18fa0c9274e085e917e767
Sergey Novikov <novikov359@gmail.com>
2016-02-16 22:22:19 +0300

    Remove show method from resources routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Cms::Engine.routes.draw do
   Cms::Configuration.resources_names.each do |resource_name|
-    resources resource_name
+    resources resource_name, except: :show
   end
 
   root 'application#boards'
