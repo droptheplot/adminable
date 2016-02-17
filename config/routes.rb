07c160a7f6699563299771f8c656063053d3ca65
Sergey Novikov <novikov359@gmail.com>
2016-02-17 17:22:34 +0300

    Add path to resources routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Cms::Engine.routes.draw do
   Cms::Configuration.resources_names.each do |resource_name|
-    resources resource_name.split('/').last, except: :show, controller: resource_name
+    resources resource_name.split('/').last, except: :show, path: resource_name, controller: resource_name
   end
 
   root 'application#boards'
