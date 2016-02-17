5ce4ceefa5f57346d126214b1c109a874e74e40f
Sergey Novikov <novikov359@gmail.com>
2016-02-17 14:38:23 +0300

    Add resources controller to ActionDispatch::Reloader

--- lib/cms/engine.rb
+++ lib/cms/engine.rb
@@ -1,5 +1,9 @@
 module Cms
   class Engine < ::Rails::Engine
     isolate_namespace Cms
+
+    ActionDispatch::Reloader.to_prepare do
+      require_dependency Cms::Engine.root.join('app/controllers/cms/resources_controller.rb')
+    end
   end
 end
