6c678f41ea495e52c86a09f956e485cdfc3f467c
Sergey Novikov <novikov359@gmail.com>
2016-02-19 00:08:24 +0300

    Update pages controller

--- app/controllers/cms/pages_controller.rb
+++ app/controllers/cms/pages_controller.rb
@@ -1,15 +1,5 @@
 module Cms
   class PagesController < ResourcesController
-    private
-
-      def columns_for_list
-        @columns_for_list = @resource.columns.select do |column|
-          %w(id title slug body created_at updated_at).include?(column.name)
-        end
-      end
-
-      def resource_model
-        Cms::Page
-      end
+    RESOURCE_MODEL = Cms::Page
   end
 end
