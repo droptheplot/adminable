ed3b8813f6a1d4f43e288a33518e00e9ce42085b
Sergey Novikov <novikov359@gmail.com>
2016-02-19 16:16:17 +0300

    Update pages feature spec

--- spec/features/pages_spec.rb
+++ spec/features/pages_spec.rb
@@ -1,8 +1,22 @@
-require 'spec_helper'
-
 describe 'Pages', type: :feature do
+  let!(:cms_page) { FactoryGirl.create(:page) }
+
   it 'index page should return status 200' do
     visit pages_path
+
+    expect(page.status_code).to eq(200)
+    expect(page).to have_content(cms_page.title)
+  end
+
+  it 'edit page should return status 200' do
+    visit edit_page_path(cms_page)
+
+    expect(page.status_code).to eq(200)
+    expect(page).to have_content(cms_page.title)
+  end
+
+  it 'new page should return status 200' do
+    visit new_page_path
 
     expect(page.status_code).to eq(200)
   end
