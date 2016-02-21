b6fb4a0381a6b79c8e242fe1fb16f827af44ecc7
Sergey Novikov <novikov359@gmail.com>
2016-02-21 11:49:34 +0300

    Move index and form partials paths to Attribute class

--- lib/cms/attribute/base.rb
+++ lib/cms/attribute/base.rb
@@ -1,8 +1,8 @@
 module Cms
   module Attribute
-    class Base      
+    class Base
       attr_accessor :name
-      attr_reader :type, :key, :klass, :strong_parameter
+      attr_reader :type, :key, :klass, :strong_parameter, :index_partial_path, :form_partial_path
 
       def initialize(name, options = {})
         @name = name
@@ -20,6 +20,14 @@ module Cms
 
       def strong_parameter
         @key
+      end
+
+      def index_partial_path
+        "index/#{ self.type }"
+      end
+
+      def form_partial_path
+        "cms/resources/form/#{ self.type }"
       end
     end
   end
