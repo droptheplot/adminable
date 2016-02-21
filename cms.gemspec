32e6bbae05ae509b8aceaadbefd27166c58aa734
Sergey Novikov <novikov359@gmail.com>
2016-02-22 00:32:29 +0300

    Update capybara version, fix pages features spec

--- cms.gemspec
+++ cms.gemspec
@@ -28,7 +28,7 @@ Gem::Specification.new do |s|
 
   s.add_development_dependency 'sqlite3'
   s.add_development_dependency 'rspec-rails'
-  s.add_development_dependency 'capybara'
+  s.add_development_dependency 'capybara', '~> 2.6.2'
   s.add_development_dependency 'factory_girl_rails'
   s.add_development_dependency 'database_cleaner'
 end
--- spec/features/pages_spec.rb
+++ spec/features/pages_spec.rb
@@ -1,23 +1,48 @@
-describe 'Pages', type: :feature do
-  let!(:cms_page) { FactoryGirl.create(:page) }
-
-  it 'index page should return status 200' do
-    visit pages_path
+feature 'Pages' do
+  scenario 'user create page' do
+    visit(new_page_path)
 
     expect(page.status_code).to eq(200)
-    expect(page).to have_content(cms_page.title)
+
+    fill_in(:page_title, with: 'New Page Title')
+    fill_in(:page_body, with: 'New Page Body')
+
+    click_button('Submit')
+
+    expect(page.current_path).to eq(pages_path)
+    expect(page).to have_content('New Page Title')
+    expect(page).to have_content('New Page Body')
+    expect(page).to have_content('Successfully Created.')
   end
 
-  it 'edit page should return status 200' do
-    visit edit_page_path(cms_page)
+  scenario 'user update page' do
+    cms_page = FactoryGirl.create(:page)
+
+    visit(edit_page_path(cms_page))
 
     expect(page.status_code).to eq(200)
-    expect(page).to have_content(cms_page.title)
+
+    fill_in(:page_title, with: 'Another Page Title')
+    fill_in(:page_body, with: 'Another Page Body')
+
+    click_button('Submit')
+
+    expect(page.current_path).to eq(pages_path)
+    expect(page).to have_content('Another Page Title')
+    expect(page).to have_content('Another Page Body')
+    expect(page).to have_content('Successfully Updated.')
   end
 
-  it 'new page should return status 200' do
-    visit new_page_path
+  scenario 'user delete page' do
+    cms_page = FactoryGirl.create(:page)
+
+    visit(pages_path)
 
     expect(page.status_code).to eq(200)
+
+    click_link('Delete')
+
+    expect(page.current_path).to eq(pages_path)
+    expect(page).to have_content('Successfully Destroyed.')
   end
 end
