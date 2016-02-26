4e12dbeec3068904f4cb49253a273fdd9d4d06cb
Sergey Novikov <novikov359@gmail.com>
2016-02-26 19:07:25 +0300

    Allow namespaced controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Cms::Engine.routes.draw do
-  Cms::Configuration.resources_names.each do |resource_name|
-    resources resource_name.split('/').last, except: :show, path: resource_name, controller: resource_name
+  Cms::Configuration.resources.each do |resources|
+    resources resources[:name], except: :show, path: resources[:path], controller: resources[:path]
   end
 
   root 'dashboard#index'
--- lib/cms/configuration.rb
+++ lib/cms/configuration.rb
@@ -1,14 +1,18 @@
 module Cms
   module Configuration
-    def self.resources_names
+    def self.resources
       resources_paths.map do |resource_path|
-        Pathname.new(resource_path).sub_ext('').each_filename.to_a
-          .split{ |file| file === 'cms' }.last.join('/').sub(/_controller$/, '')
+        resource = resource_path.to_s.split('cms/').last.sub(/_controller\.rb$/, '')
+
+        {
+          name: resource.tr('/', '_'),
+          path: resource
+        }
       end
     end
 
     def self.resources_paths
-      Dir[Rails.root.join('app/controllers/cms/*_controller.rb')] <<
+      Dir[Rails.root.join('app/controllers/cms/**/*_controller.rb')] <<
       Cms::Engine.root.join('app/controllers/cms/pages_controller.rb')
     end
   end
