8a87299e4b115345bf063fce5d156038daa19014
Sergey Novikov <novikov359@gmail.com>
2016-03-18 19:16:47 +0300

    Add clusterize, change has_many to list

---
+++ app/assets/javascripts/cms/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  if($('#clusterizeScrollArea').length && $('#clusterizeContentArea').length) {
+    var clusterize = new Clusterize({
+      scrollId: 'clusterizeScrollArea',
+      contentId: 'clusterizeContentArea'
+    });
+  };
+
+  $('.toggleCheckbox').click(function() {
+    var checkbox = $(this).children('input[type=checkbox]');
+    checkbox.prop('checked', !checkbox.prop('checked'));
+    $(this).toggleClass('active');
+  });
+});
