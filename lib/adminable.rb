f6f51c7c3ce247e584de68ecd567dd1aa3efbca0
Sergey Novikov <novikov359@gmail.com>
2016-04-06 16:25:16 +0300

    Remove coffee-rails gem

--- lib/adminable.rb
+++ lib/adminable.rb
@@ -22,7 +22,6 @@ require 'adminable/attributes/types/text'
 
 require 'haml-rails'
 require 'sass-rails'
-require 'coffee-rails'
 require 'jquery-rails'
 require 'bootstrap'
 require 'rails-assets-tether'
