0647c6d1d27931b5b6284e39d18e96fd728eaa0d
Sergey Novikov <novikov359@gmail.com>
2016-04-16 10:57:59 +0300

    Add specs for Attributes::Association class

---
+++ spec/lib/attributes/association_spec.rb
@@ -0,0 +1,19 @@
+describe Adminable::Attributes::Association do
+  let!(:reflection) do
+    User.reflect_on_all_associations.find { |a| a.name == :blog_posts }
+  end
+  let!(:association) { Adminable::Attributes::Association.new(reflection) }
+  let!(:blog_post) { FactoryGirl.create(:blog_post) }
+
+  describe '@model' do
+    it 'has ResourceConcern included' do
+      expect(association.model.first).to respond_to(:adminable_name)
+    end
+  end
+
+  describe '#options_for_select' do
+    it 'returns all entries for association model' do
+      expect(association.options_for_select).to match_array(blog_post)
+    end
+  end
+end
