b00acbf237495c93981cdb83594ae4927409285e
Sergey Novikov <novikov359@gmail.com>
2016-04-20 21:47:01 +0300

    Add mount_engine to install generator

--- lib/generators/adminable/install_generator.rb
+++ lib/generators/adminable/install_generator.rb
@@ -11,6 +11,13 @@ module Adminable
           'app/controllers/adminable/application_controller.rb'
         )
       end
+
+      def mount_engine
+        insert_into_file "#{Rails.root}/config/routes.rb", :after => /routes.draw.do\n/ do
+          %Q{  mount Adminable::Engine, at: '/adminable'
+}
+        end
+      end
     end
   end
 end
