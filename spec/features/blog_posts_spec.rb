c83b9de65bf2c1c15ddf461c0cddc80312a25afe
Sergey Novikov <novikov359@gmail.com>
2016-04-21 11:08:59 +0300

    Add errors to blog post spec

--- spec/features/blog_posts_spec.rb
+++ spec/features/blog_posts_spec.rb
@@ -1,43 +1,63 @@
 feature 'Blog Posts' do
-  scenario 'user create blog post' do
-    FactoryGirl.create(:user)
+  feature 'user creates blog post' do
 
-    visit(new_blog_post_path)
+    before do
+      FactoryGirl.create(:user)
+      visit(new_blog_post_path)
+      expect(page.status_code).to eq(200)
+    end
 
-    expect(page.status_code).to eq(200)
+    scenario 'with required fields' do
+      fill_in(:blog_post_title, with: 'New Post Title')
+      fill_in(:blog_post_body, with: 'New Post Body')
+      choose('blog_post_user_id_1')
 
-    fill_in(:blog_post_title, with: 'New Post Title')
-    fill_in(:blog_post_body, with: 'New Post Body')
-    choose('blog_post_user_id_1')
+      click_button('Submit')
 
-    click_button('Submit')
+      expect(page.current_path).to eq(blog_posts_path)
+      expect(page).to have_content('New Post Title')
+      expect(page).to have_content('New Post Body')
+      expect(page).to have_content(
+        I18n.t('adminable.resources.created', resource: 'Post')
+      )
+    end
 
-    expect(page.current_path).to eq(blog_posts_path)
-    expect(page).to have_content('New Post Title')
-    expect(page).to have_content('New Post Body')
-    expect(page).to have_content(
-      I18n.t('adminable.resources.created', resource: 'Post')
-    )
+    scenario 'without required fields' do
+      click_button('Submit')
+
+      expect(page.current_path).to eq(blog_posts_path)
+      expect(page).to have_content("Title can't be blank")
+    end
   end
 
-  scenario 'user update blog post' do
-    blog_post = FactoryGirl.create(:blog_post)
+  feature 'user updates blog post' do
+    before do
+      blog_post = FactoryGirl.create(:blog_post)
+      visit(edit_blog_post_path(blog_post))
+      expect(page.status_code).to eq(200)
+    end
 
-    visit(edit_blog_post_path(blog_post))
+    scenario 'with required fields' do
+      fill_in(:blog_post_title, with: 'Another Post Title')
+      fill_in(:blog_post_body, with: 'Another Post Body')
 
-    expect(page.status_code).to eq(200)
+      click_button('Submit')
 
-    fill_in(:blog_post_title, with: 'Another Post Title')
-    fill_in(:blog_post_body, with: 'Another Post Body')
+      expect(page.current_path).to eq(blog_posts_path)
+      expect(page).to have_content('Another Post Title')
+      expect(page).to have_content('Another Post Body')
+      expect(page).to have_content(
+        I18n.t('adminable.resources.updated', resource: 'Post')
+      )
+    end
 
-    click_button('Submit')
+    scenario 'without required fields' do
+      fill_in(:blog_post_title, with: '')
 
-    expect(page.current_path).to eq(blog_posts_path)
-    expect(page).to have_content('Another Post Title')
-    expect(page).to have_content('Another Post Body')
-    expect(page).to have_content(
-      I18n.t('adminable.resources.updated', resource: 'Post')
-    )
+      click_button('Submit')
+
+      expect(page).to have_content("Title can't be blank")
+    end
   end
 
   scenario 'user delete blog post' do
