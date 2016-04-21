9a7bdb63b2e2f60c3d9374fa092aa5fca3de7436
Sergey Novikov <novikov359@gmail.com>
2016-04-21 21:02:33 +0300

    Refactor resource concern adminable method

--- app/models/concerns/adminable/resource_concern.rb
+++ app/models/concerns/adminable/resource_concern.rb
@@ -4,7 +4,11 @@ module Adminable
 
     def adminable
       %i(title name email login id).each do |name|
-        return OpenStruct.new(name: public_send(name)) unless try(name).nil?
+        begin
+          return OpenStruct.new(name: public_send(name))
+        rescue NoMethodError
+          next
+        end
       end
     end
   end
