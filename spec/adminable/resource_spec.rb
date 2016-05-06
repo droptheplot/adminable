a7b4c8e67f1ae7043c2603de7bf3285d4d1f60e8
Sergey Novikov <novikov359@gmail.com>
2016-05-07 01:14:45 +0300

    Add spec for resource route method

--- spec/adminable/resource_spec.rb
+++ spec/adminable/resource_spec.rb
@@ -18,6 +18,12 @@ describe Adminable::Resource do
     end
   end
 
+  describe '#route' do
+    it 'returns correct route' do
+      expect(Adminable::Resource.new('blog/posts').route).to eq('blog_posts')
+    end
+  end
+
   describe '#==' do
     it 'returns true for resource with same name' do
       expect(Adminable::Resource.new('users')).to eq(
